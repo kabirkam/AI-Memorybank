f303c91fe16584c843112cc1d52209a97150c91e
Aamir Shaikh <aamir0744@gmail.com>
2023-03-06 17:49:34 +0000

    moving test audio display code to test page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/test', to: "pages#test"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
