804995d1ef7663518a234e6669b8f6f623dd8361
Aamir Shaikh <aamir0744@gmail.com>
2023-03-16 22:30:02 +0000

    adding real sounding user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ User.destroy_all
 
 puts 'creating fake users'
 user1 = User.create!(email: 'test1@test.com', password: 'testing')
-user2 = User.create!(email: 'test2@test.com', password: 'testing')
+user2 = User.create!(email: 'faizan@gmail.com', password: 'kabir')
 
 # puts 'creating fake category'
 # category1 = Category.create!(name: "Work")
@@ -19,7 +19,7 @@ puts 'creating fake notes'
 sample_text = "Remember to call back Mum. She had mentioned in our last call that she was stressed about work because she couldn't find anyone to install the light fixture for a job. Also ask her how her friend Asma is doing."
 note1 = Note.new(text: sample_text)
 note1.user = user1
-note1.sentences.push(sample_text.split(/[,.]/).map { |sent| Sentence.new(text: sent) })
+note1.sentences.push(sample_text.split(/[,.?!]/).map { |sent| Sentence.new(text: sent) })
 # note1.category = category1
 note1.audio.attach(
   filename: "test-audio1.mp3",
